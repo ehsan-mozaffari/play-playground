070d027d3e7e77e80c8679973efe570cb797f94c
Ehsan Mozaffari <ehsan.mozaffari@yahoo.com>
2019-12-28 01:48:45 +0330

    add error handler in progress

---
+++ app/ErrorHandler.scala
@@ -0,0 +1,26 @@
+import javax.inject.Singleton
+import play.api.Environment
+import play.api.http.JsonHttpErrorHandler
+import play.api.http.Status.{BAD_REQUEST, FORBIDDEN, NOT_FOUND}
+import play.api.mvc.{RequestHeader, Result}
+import play.core.SourceMapper
+
+import scala.concurrent.Future
+
+@Singleton
+class ErrorHandler(environment: Environment, sourceMapper: Option[SourceMapper] = None)
+  extends JsonHttpErrorHandler(environment: Environment, sourceMapper: Option[SourceMapper]) {
+
+//  override def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = statusCode match {
+//    case BAD_REQUEST => onBadRequest(request, message)
+//    case FORBIDDEN => onForbidden(request, message)
+//    case NOT_FOUND => onNotFound(request, message)
+//    case clientError if statusCode >= 400 && statusCode < 500 => onOtherClientError(request, statusCode, message)
+//    case nonClientError =>
+//      throw new IllegalArgumentException(
+//        s"onClientError invoked with non client error status code $statusCode: $message"
+//      )
+//  }
+
+  override def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = super.onServerError(request, exception)
+}
--- app/controllers/StudentController.scala
+++ app/controllers/StudentController.scala
@@ -4,24 +4,27 @@ import java.time.LocalDate
 
 import daos.StudentDao
 import javax.inject._
-import play.api.libs.json.Json
+import play.api.libs.json.{JsError, Json, Reads}
 import play.api.mvc._
+
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class StudentController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class StudentController @Inject()(val controllerComponents: ControllerComponents)(implicit ec: ExecutionContext) extends BaseController {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def getStudentById(id: Long) = Action { implicit request: Request[AnyContent] =>
+  def validateJson[A: Reads]: BodyParser[A] = parse.json.validate { x =>
+     x.validate[A].asEither.left.map(e => BadRequest(JsError.toJson(e)))
+  }
+
+  def getStudentById(id: Long) = Action/*.async(validateJson[Long]) */{ implicit request: Request[AnyContent] =>
     Ok(Json.toJson[StudentDao](StudentDao(id = id, name = "John Doe", age = 25, gender = "Male", birthDate = LocalDate.parse("1990-02-26"))))
+  }
+
+  def addStudent: Action[StudentDao] = Action.async(validateJson[StudentDao]) { implicit request =>
+    Future.successful(Ok(Json.toJson("added")))
   }
 }
